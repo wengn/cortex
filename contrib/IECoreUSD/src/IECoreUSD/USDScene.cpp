9b3dfe7b4ed1b2f37ee8fc6bdeb0e889eb6d77a6
zhangxinlong <zxl120125@126.com>
2020-08-06 17:34:57 +0800

    GAF-274:add camera import support,GAF-261:add instance support

--- contrib/IECoreUSD/src/IECoreUSD/USDScene.cpp
+++ contrib/IECoreUSD/src/IECoreUSD/USDScene.cpp
@@ -31,7 +31,6 @@
 //  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 //////////////////////////////////////////////////////////////////////////
-
 #include "USDScene.h"
 
 #include "IECoreScene/Camera.h"
@@ -1039,6 +1038,66 @@ IECoreScene::ExternalProceduralPtr convertRenderProxy(pxr::UsdPrim variantSet, p
   return result;
 }
 
+IECore::ConstObjectPtr convertCamera( pxr::UsdGeomCamera camera, pxr::UsdTimeCode time)
+{
+    float horizontalAper,  horizontalOff, verticalAper, verticalOff;
+    camera.GetHorizontalApertureAttr().Get(&horizontalAper, time);
+    camera.GetVerticalApertureAttr().Get(&verticalAper, time);
+    camera.GetHorizontalApertureOffsetAttr().Get(&horizontalOff, time);
+    camera.GetVerticalApertureOffsetAttr().Get(&verticalOff, time);
+
+    IECoreScene::CameraPtr result = new IECoreScene::Camera;
+    pxr::TfToken  projToken;
+    camera.GetProjectionAttr().Get(&projToken);
+
+    if(projToken == pxr::UsdGeomTokens->perspective)
+    {
+        result->setProjection("perspective");
+
+        // We store focalLength and aperture in arbitary units.  USD uses tenths
+        // of scene units
+        float scale = 10.0f * result->getFocalLengthWorldScale();
+
+        float focalLengthVal;
+        camera.GetFocalLengthAttr().Get( &focalLengthVal, time );
+        result->setFocalLength(focalLengthVal / scale);
+        result->setAperture(Imath::V2f(horizontalAper / scale, verticalAper / scale));
+        result->setApertureOffset(Imath::V2f(horizontalOff / scale,  verticalOff / scale));
+
+    }
+    else if (projToken == pxr::UsdGeomTokens->orthographic)
+    {
+        result->setProjection("orthographic");
+
+        // For ortho cameras, USD uses aperture units of tenths of scene units
+        result->setAperture(Imath::V2f(horizontalAper / 10.0f, verticalAper / 10.0f));
+        result->setApertureOffset(Imath::V2f(horizontalOff / 10.0f, verticalOff / 10.0f));
+
+     }
+    else { }
+
+    pxr::VtValue clipRangeVal;
+    camera.GetClippingRangeAttr().Get(&clipRangeVal, time);
+    pxr::GfVec2f clipRange = clipRangeVal.Get<pxr::GfVec2f>();
+    result->setClippingPlanes(Imath::V2f(clipRange[0], clipRange[1]));
+
+    float fStop;
+    camera.GetFStopAttr().Get(&fStop, time);
+    result->setFStop(fStop);
+
+    float focusDis;
+    camera.GetFocusDistanceAttr().Get(&focusDis, time);
+    result->setFocusDistance(focusDis);
+
+    double shutterOpen, shutterClose;
+    camera.GetShutterOpenAttr().Get(&shutterOpen, time);
+    camera.GetShutterCloseAttr().Get(&shutterClose, time);
+    result->setShutter(Imath::V2f(shutterOpen, shutterClose));
+
+    return result;
+
+}
+
 IECoreScene::MeshPrimitivePtr convertPrimitive( pxr::UsdGeomMesh mesh, pxr::UsdTimeCode time )
 {
 	pxr::UsdAttribute subdivSchemeAttr = mesh.GetSubdivisionSchemeAttr();
@@ -1307,9 +1366,14 @@ bool isConvertible( pxr::UsdPrim prim )
 
 	pxr::UsdGeomSphere sphere( prim );
 	if ( sphere )
-	{
-		return true;
-	}
+    	{
+        	return true;
+    	}
+    	pxr::UsdGeomCamera camera( prim );
+    	if( camera )
+    	{
+        	return true;
+    	}
 
     // Special handling of "ALMayaReference" node in variantSet
     // "ALMayaReference" and "mayaReference" are values that are hardcoded in Anim logic exporting
@@ -1369,7 +1433,13 @@ IECore::ConstObjectPtr convertPrimitive( pxr::UsdPrim prim, pxr::UsdTimeCode tim
             return convertRenderProxy( prim, time );
     }
 
+	if( pxr::UsdGeomCamera camera = pxr::UsdGeomCamera( prim ))
+    {
+        return convertCamera( camera, time );
+    }
+    
     return nullptr;
+    
 }
 
 bool hasTimeVaryingPrimVars( pxr::UsdGeomImageable imagable )
@@ -2294,15 +2364,23 @@ void USDScene::attributeHash ( double time, IECore::MurmurHash &h) const
 void USDScene::objectHash( double time, IECore::MurmurHash &h ) const
 {
 	if( isConvertible( m_location->prim ) )
-	{
-		h.append( m_location->prim.GetPath().GetString() );
-		h.append( m_root->fileName() );
+    {
+        if( m_location->prim.IsInstance())
+        {
+            h.append( m_location->prim.GetMaster().GetPath().GetString());
+            h.append( m_root->fileName());
+        }
+        else
+         {
+            h.append( m_location->prim.GetPath().GetString() );
+            h.append( m_root->fileName() );
+        }
 
-		if( isTimeVarying( m_location->prim ) )
-		{
-			h.append( time );
-		}
-	}
+        if( isTimeVarying( m_location->prim ) )
+        {
+            h.append( time );
+        }
+    }
 }
 void USDScene::childNamesHash( double time, IECore::MurmurHash &h ) const
 {
